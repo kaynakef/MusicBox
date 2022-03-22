22bab110a1225adb3bfda9feebe9531992b799fb
kaynakef <99458247+kaynakef@users.noreply.github.com>
2022-03-22 10:26:11 -0400

    Add files via upload

---
+++ MusicBox.java
@@ -0,0 +1,125 @@
+package MusicBox;
+
+import java.util.Scanner;
+
+public class MusicBox {
+    public static void main(String[] args) {
+        //March 7th 2022 --- MUsic Box-- Team Tenacious Tigers
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the music types: ");
+        String musicTypes = scanner.nextLine().toLowerCase().trim();
+
+        switch (musicTypes) {
+            default:
+                System.out.println(musicTypes+" music category is not available.");
+            case "classical":
+                System.out.println("Which song would you like to listen? \n Four Seasons \n Fur Elise \n Finlandia \n Vocalize \n The Planets ");
+                String classicalSong = scanner.nextLine().toLowerCase().trim();
+                switch (classicalSong) {
+                    case "four seasons":
+                    case "fur elise":
+                    case "finlandia":
+                    case "vocalise":
+                    case "the planets":
+                        // add the $$$$$
+                        System.out.println("Please provide payment for song, cost of song is $2.00");
+                        double costClassical = scanner.nextDouble();
+                        if (costClassical == 2) {
+                            System.out.println("You are listening to " + classicalSong + ". Enjoy the song!");
+                        } else if(costClassical < 2)
+                        {while (costClassical < 2) {
+                            System.out.println("Entered price is not enough, please add $"+ (2-costClassical)+ " more");
+                            costClassical= scanner.nextDouble()+costClassical;}
+                            if (costClassical>2){
+                                System.out.println("You have entered $" + (costClassical - 2) + " more than required. Please enjoy the music while we processing your change!");
+                                System.out.println("Here is your $"+ (costClassical - 2)+ "change.");
+                            }else{
+                                System.out.println("You are listening to " +costClassical + ". Enjoy the song!");
+                            }
+
+                        } else {
+                            System.out.println("You have entered $" + (costClassical - 2) + " more than required. Please enjoy the music while we processing your change!");
+                            System.out.println("Here is your $"+ (costClassical - 2)+ "change.");
+                        }
+                        break;
+                    default:
+                        System.out.println("The " + classicalSong + " is not available.");
+                        break;
+
+                } break;
+            case "pop":
+                System.out.println("Which song would you like to listen? \n Bad Guy \n Talk \n Please Me \n 7 ring \n Without Me ");
+                String popSong = scanner.nextLine().toLowerCase().trim();
+                switch (popSong) {
+                    case "bad guy":
+                    case "talk":
+                    case "please me":
+                    case "7 ring":
+                    case "without me":
+                        System.out.println("Please provide payment for song, cost of song is $4.00");
+                        double costPop = scanner.nextDouble();
+                        if (costPop == 4) {
+                            System.out.println("You are listening to " + popSong + ". Enjoy the song!");
+                        } else if (costPop < 4) {
+                            while (costPop<4){
+                                System.out.println("Entered price is not enough, please add $"+ (4-costPop)+ " more");
+                                costPop= scanner.nextDouble()+costPop;}
+                            if (costPop>4) {
+                                System.out.println("You have entered $" + (costPop - 4) + " more than required. Please enjoy the music while we processing your change!");
+                                System.out.println("Here is your $" + (costPop - 4) + " change.");
+                            }else{
+                                System.out.println("You are listening to " + popSong + ". Enjoy the song!");
+                            }
+                        }
+
+                else {
+                            System.out.println("You have entered $" + (costPop - 4) + " more than required. Please enjoy the music while we processing your change!");
+                            System.out.println("Here is your $" + (costPop - 4) + " change.");
+                        }
+                        break;
+                    default:
+                        System.out.println("The" + popSong + "is not available song.");
+                        break;
+
+                }break;
+            case "country":
+                System.out.println("Which song would you like to listen to? \n Meant To Be \n Heaven \n Simple \n One Number Away \n Get Along");
+                String countrySong = scanner.nextLine().toLowerCase().trim();
+                switch (countrySong) {
+                    case "heaven":
+                    case "simple":
+                    case "one number away":
+                    case "get along":
+                    case "meant to be":
+                        System.out.println("Please provide payment for song, cost of song is $3.00");
+                        double costCountry = scanner.nextDouble();
+                        if (costCountry == 3) {
+                            System.out.println("You are listening to " + countrySong + ". Enjoy the song!");
+                        } else if (costCountry < 3) {
+                            while(costCountry<3){
+                                System.out.println("Entered price is not enough, please add $"+ (3-costCountry)+ " more");
+                                costCountry= scanner.nextDouble()+costCountry;
+                                if (costCountry>3){System.out.println("You have entered $" + (costCountry - 4) + " more than required. Please enjoy the music while we processing your change!");
+                                    System.out.println("Here is your $" + (costCountry - 4) + " change.");
+
+                                } else {
+                                    System.out.println("You are listening to " + countrySong + ". Enjoy the song!");
+                                }
+                            }
+
+                        } else {
+                            System.out.println("You have entered $" + (costCountry - 3) + " more than required. Please wait for the change.");
+                        }
+                        break;
+
+                    default:
+                        System.out.println("The" + countrySong + "is not available song.");
+                        break;
+
+                }break;
+
+
+        }
+    }
+}
